5e924216f9adb2cd02e382178ec56d6fe754ccb4
Tiago Costa <tiagodpv@gmail.com>
2022-02-21 14:45:42 -0500

    colors

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { RepositoryList } from './components/RepositoriesList';
+
+import './styles/global.scss';
+
+export function App(){
+
+    return (        
+            <RepositoryList/>      
+    )
+}
